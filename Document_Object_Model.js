e29daec1e15cb46b61d01c170132a82705a85bca
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2025-06-22 22:05:52 +0530

    Lec-3

---
+++ Document_Object_Model.js
@@ -0,0 +1,27 @@
+console.log("DOM");
+
+// console.log(document); // he purna document access krun dein....
+// console.log(document.documentElement); // he tumhala <html> te </html> madhla purna access krun dein...
+// console.log(document.head); // hyani tumhi head element access kru shakal
+// console.log(document.body); // hyani tumhi body element access kru shakal
+// console.log(document.title); // hyani tumhi website cha current title console vr print kru shakal
+
+
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+// console.log(Array.from(document.body.children));
+
+
+
+// let div = Array.from(document.body.children);
+// div[1].innerHTML = "<strike>this line is written in the bold tag</strike>";
+// console.log(div[1])
+
+
+
+
+// let h2 = document.getElementById("id1");
+// let div = document.getElementsByClassName("paragraph");
+// // h2.innerHTML = "<h2><b>Helllooooo javascirpt</b></h2>";
+// // h2.style.color = "purple";
+// console.log(div);
