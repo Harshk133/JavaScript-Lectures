3515df9442914c9fe7aa8c4b302281cf08edf79d
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2025-06-23 21:20:03 +0530

    Lec-4

---
+++ 6_Array.js
@@ -0,0 +1,57 @@
+console.log("Array");
+// array is collection of values
+
+// creation of array
+
+// let students = [1,2,3,4,5,"string",true,false,[1,2],{k:"v",a:"b"},null];
+// console.log(students);
+
+// // updating the array value
+// students[0] = 0;
+// console.log(students);
+
+
+// Methods of Array
+// property 
+let myArr = [1,2,34,5,6,7];
+
+
+// console.log("the number of elements in the array are:- ", myArr);
+// console.log("the number of elements in the array are:- ", myArr.length);
+
+// method
+// syntax:- arr.function();
+// toString()
+// console.log(typeof myArr);
+// let newArr = myArr.toString();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// join(pattern)
+// console.log(myArr.join("<--->"));
+
+// push() - this will add the element in the last of array
+// myArr.push(100);
+// myArr.push(200);
+// myArr.push(2000);
+// myArr.push("harsh");
+// myArr.push([1,2]);
+// console.log(myArr);
+
+// pop() - this will delete the last element
+// console.log(myArr);
+// myArr.pop();
+// myArr.pop();
+// console.log(myArr);
+
+// unshift(element) - this will add the new element in the first position in the array
+// console.log(myArr);
+// myArr.unshift(100);
+// myArr.unshift("this is the string");
+// console.log(myArr);
+
+// shift() - this will delete the first element or the zeroth element in the array.
+// console.log(myArr);
+// console.log(myArr.shift());
+// console.log(myArr.shift());
+// console.log(myArr);
